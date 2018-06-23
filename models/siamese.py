f818cd979e60ec6305cdadb66e02059f5721b062
Ubuntu <ubuntu@ip-172-31-0-100.us-west-2.compute.internal>
2018-06-23 00:28:06 +0000

    add batch norm to siamese network

--- models/siamese.py
+++ models/siamese.py
@@ -9,15 +9,19 @@ class Siamese(nn.Module):
         super(Siamese, self).__init__()
 
         self.fc1 = nn.Linear(200, 400)
+        self.bn1 = nn.BatchNorm1d(400)
         self.fc2 = nn.Linear(400, 200)
+        self.bn2 = nn.BatchNorm1d(200)
         self.fc3 = nn.Linear(200, 100)
+        self.bn3 = nn.BatchNorm1d(100)
         self.fc4 = nn.Linear(100, 2)
+        self.bn4 = nn.BatchNorm1d(2)
         
     def forward(self, x):
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
-        x = self.fc4(x)
+        x = F.leaky_relu(self.bn1(self.fc1(x)))
+        x = F.leaky_relu(self.bn2(self.fc2(x)))
+        x = F.leaky_relu(self.bn3(self.fc3(x)))
+        x = self.bn4(self.fc4(x))
         return F.sigmoid(x)
 
 
